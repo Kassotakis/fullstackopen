f72ca1a7e78c389a28424f8aecee086ae2b822bb
kassotakis <kassotakisnikolas@gmail.com>
2025-05-17 07:17:27 +0300

    2.11 end

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Jay-Z', number: '040-123456', id: 1 },
-    { name: 'Jenny Lopez', number: '39-44-5323523', id: 2 },
-    { name: 'Taylor Swift', number: '12-43-234345', id: 3 },
-    { name: 'Kanye West', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
