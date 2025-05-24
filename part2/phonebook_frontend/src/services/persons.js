e0ff5a041767ab63d81f825ca3ed09aea88c6d0c
kassotakis <kassotakisnikolas@gmail.com>
2025-05-24 22:54:57 +0300

    Changed data fetching route

--- part2/phonebook_frontend/src/services/persons.js
+++ part2/phonebook_frontend/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => axios.get(baseUrl).then(res => res.data)
 const create = newPerson => axios.post(baseUrl, newPerson).then(res => res.data)
