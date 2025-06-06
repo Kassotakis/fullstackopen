95010a6b660083020e1dcd464b22aa3006e2960d
kassotakis <kassotakisnikolas@gmail.com>
2025-06-06 10:01:11 +0300

    5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const detailsStyle = {
+    display: visible ? '' : 'none'
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} by {blog.author}
+        <button onClick={toggleVisibility}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+
+      <div style={detailsStyle}>
+        {blog.url}<br />
+        likes: {blog.likes} <button>like</button><br />
+        added by {blog.user?.name || 'unknown'}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
