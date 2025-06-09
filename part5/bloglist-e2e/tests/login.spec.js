8f7bc1cc006759d666a3366f01cc39b834a25788
kassotakis <kassotakisnikolas@gmail.com>
2025-06-09 12:27:22 +0300

    5.19

--- part5/bloglist-e2e/tests/login.spec.js
+++ part5/bloglist-e2e/tests/login.spec.js
@@ -1,3 +1,5 @@
+const { loginWith } = require('./helper')
+
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
@@ -22,9 +24,7 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-        await page.getByTestId('username').fill('Yeezy')
-        await page.getByTestId('password').fill('inparis')
-        await page.getByRole('button', { name: 'login' }).click()
+        await loginWith(page, 'Yeezy', 'inparis')
 
         await expect(page.getByText('Login Successful')).toBeVisible()
         await expect(page.getByText('Kanye West logged in')).toBeVisible()
@@ -38,4 +38,21 @@ describe('Blog app', () => {
         await expect(page.getByText('Wrong Credentials')).toBeVisible()
     })
   })
+
+  describe('When logged in', () => {
+  beforeEach(async ({ page }) => {
+    await loginWith(page, 'Yeezy', 'inparis')
+  })
+
+  test('a new blog can be created', async ({ page }) => {
+    await page.getByRole('button', { name: 'Create new blog' }).click()
+    await page.getByLabel('title:').fill('Kanye West in Paris')
+    await page.getByLabel('author:').fill('Ye')
+    await page.getByLabel('url:').fill('Yessir')
+    await page.getByRole('button', { name: 'create' }).click()
+
+    await expect(page.getByText('Added blog "Kanye West in Paris" by Ye')).toBeVisible()
+    await expect(page.getByText('Kanye West in Paris by Ye')).toBeVisible()
+  })
+})
 })
--- part5/bloglist-frontend/src/components/BlogForm.jsx
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -15,11 +15,33 @@ const BlogForm = ({ createBlog }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>title: <input value={title} onChange={({ target }) => setTitle(target.value)} /></div>
-      <div>author: <input value={author} onChange={({ target }) => setAuthor(target.value)} /></div>
-      <div>url: <input value={url} onChange={({ target }) => setUrl(target.value)} /></div>
+      <div>
+        <label htmlFor="title">title:</label>
+        <input
+          id="title"
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="author">author:</label>
+        <input
+          id="author"
+          value={author}
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="url">url:</label>
+        <input
+          id="url"
+          value={url}
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
       <button type="submit">create</button>
     </form>
+
   )
 }
 
